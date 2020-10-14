84505e79aab2eb2ddce089ff7477d9c6168d7458
Omkar <Omkar@192.168.0.15>
2020-10-14 18:05:36 -0400

    SpringBootServletInitializer interface added

--- appRegistration/src/main/java/com/example/appRegistration/AppRegistrationApplication.java
+++ appRegistration/src/main/java/com/example/appRegistration/AppRegistrationApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AppRegistrationApplication {
+public class AppRegistrationApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppRegistrationApplication.class, args);
